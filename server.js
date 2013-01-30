f8947aefd7ea418fee1367943a86b6ec1e4d4ac9
Norman Khine <khinester@aqoon.local>
2013-01-30 18:28:25 +0100

    TZM Network dropup now works, but there is a small hack to fix

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var city = new City('data/GeoLiteCity.dat' );
 
 var GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
-var TABLE_ID = "1epTUiUlv5NQK5x4sgdy1K47ACDTpHH60hbng1qw";
-//var TABLE_ID ="1obpi0bmSDILX1cIQcVRNi1lUkm2K5xBFztmRFiM"
+//var TABLE_ID = "1epTUiUlv5NQK5x4sgdy1K47ACDTpHH60hbng1qw";
+var TABLE_ID ="1obpi0bmSDILX1cIQcVRNi1lUkm2K5xBFztmRFiM"
 
 var GOOGLE_PATH = "/fusiontables/v1/query?sql=SELECT%20*%20FROM%20"+TABLE_ID+"&key="+GOOGLE_API_KEY;
 var GOOGLE_DRIVE_PATH = "/drive/v2/files/"+TABLE_ID+"?key="+GOOGLE_API_KEY;
